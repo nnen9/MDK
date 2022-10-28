6d66fec6c6a5179ebc9df4592f4b20c971f0fde5
nnen9 <gigrys.drop@gmail.com>
2022-10-28 21:07:10 +0300

    lab7 complited

---
+++ AllLabs/LabsLib/Lab5/Matrix.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LabsLib.Lab5
+{
+    internal class Matrix11
+    {
+        public void Matrix1()
+        {
+            Random rnd = new Random();
+            int[,] mass = new int[5, 5];
+            for (int i = 0; i < mass.GetLength(0); i++)
+            {
+                for (int j = 0; j < mass.GetLength(1); j++)
+                {
+                    mass[i, j] = rnd.Next(0, 100);
+                    Console.Write(mass[i, j] + "\t");
+                }
+                Console.WriteLine();
+                Console.WriteLine();
+            }
+            Console.WriteLine("Главная диагональ");
+            for (int i = 0; i < mass.GetLength(0); i++)
+            {
+                Console.WriteLine(mass[i, i] + "\t");
+            }
+            Console.WriteLine("Побочная диагональ");
+            for (int i = 0; i < mass.GetLength(0); i++)
+            {
+                Console.WriteLine(mass[mass.GetLength(1) - i - 1, i] + "\t");
+            }
+            Console.WriteLine("Сумма всех элементов");
+            int summa = 0;
+            for (int i = 0; i < mass.GetLength(0); i++)
+            {
+                for (int j = 0; j < mass.GetLength(1); j++)
+                {
+                    summa += mass[i, j];
+                }
+            }
+            Console.WriteLine(summa);
+        }
+    }
+}
---
+++ AllLabs/LabsLib/Lab7/Group.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LabsLib.Lab7
+{
+    public class Group
+    {
+        public void AddStudent(List<Student> students)
+        {
+            students = _students;
+        }
+        private static List<Student> _students = new List<Student>();
+        public static void AddStudent()
+        {
+            _students.Add(new Student());
+            AddStudentInfo();
+        }
+        private static void AddStudentInfo()
+        {
+            foreach (Student student in _students)
+            {
+                Console.WriteLine("Введите имя студента");
+                student.Name = Console.ReadLine();
+                Console.WriteLine("Введите пол студента");
+                student.Gender = Console.ReadLine();
+                Console.WriteLine("Введите возраст студента");
+                student.Age = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Введите ВУЗ/ССУЗ студента");
+                student.Institution = Console.ReadLine();
+                Console.WriteLine("Введите специальность студента");
+                student.Specialization = Console.ReadLine();
+                Console.WriteLine("Введите группу студента");
+                student.Group = Console.ReadLine();
+            }
+        }
+        public static int StudentCount()
+        {
+            int count = 0;
+            foreach (Student student in _students)
+            {
+                count++;
+            }
+
+            return count;
+        }
+        public static string StudentsInfo()
+        {
+            foreach (Student student in _students)
+            {
+                return ($"Имя: {student.Name}, Пол: {student.Gender}, Возраст: {student.Age}, ВУЗ/ССУЗ: {student.Institution}, Специальность: {student.Specialization} Группа: {student.Group}");
+            }
+            return "";
+        }
+    }
+}
